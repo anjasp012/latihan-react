7a1e38638f39e39c5d8a0d1bb4a18e7a7b57bfad
anjas putra <anjas.2104@students.amikom.ac.id>
2021-12-10 22:54:07 +0700

    Component Pertama

--- src/App.js
+++ src/App.js
@@ -1,43 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Card from './components/Card';
 
 function App() {
-
-    const [firstname, setFirstname] = useState('')
-    const [lastname, setLastname] = useState('')
-    const [name, setName] = useState('')
-
-    const submitHandler = (e) => {
-        e.preventDefault()
-        setName(`${firstname} ${lastname}`)
-
-        setFirstname('')
-        setLastname('')
-    }
-
     return (
-        <div className="p-5">
+        <div className="p-4">
             <div className="container">
+                <h3>Posts</h3>
+                <hr />
                 <div className="row">
                     <div className="col-md-4">
-                        <div className="card">
-                            <div className="card-header">Form React</div>
-                            <div className="card-body">
-                                <form onSubmit={submitHandler}>
-                                    <div className="mb-4">
-                                        <label htmlFor="firstname" className="form-label">Firstname</label>
-                                        <input type="text" value={firstname} onChange={(e) => setFirstname(e.target.value)} name="firstname" id="firstname" className="form-control" />
-                                    </div>
-                                    <div className="mb-4">
-                                        <label htmlFor="lastname" className="form-label">Lastname</label>
-                                        <input type="text" value={lastname} onChange={(e) => setLastname(e.target.value)} name="lastname" id="lastname" className="form-control" />
-                                    </div>
-                                    <div className="d-grid">
-                                        <button type="submit" className="btn btn-primary">Show</button>
-                                    </div>
-                                </form>
-                            </div>
-                            <div className="card-footer">My Name Is {name ? name : '....'}</div>
-                        </div>
+                        <Card content={{
+                            imgUrl:"http://placekitten.com/g/300/200",
+                            title: "Judul Pertama",
+                            published: "06 Des, 2021",
+                            }}/>
+                    </div>
+                    <div className="col-md-4">
+                        <Card content={{
+                            imgUrl:"http://placekitten.com/g/300/200",
+                            title: "Judul Kedua",
+                            published: "07 Des, 2021",
+                            }}/>
+                    </div>
+                    <div className="col-md-4">
+                        <Card content={{
+                            imgUrl:"http://placekitten.com/300/200",
+                            title: "Judul Ketiga",
+                            published: "08 Des, 2021",
+                            }}/>
                     </div>
                 </div>
             </div>
